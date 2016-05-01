adebfd1746ca8b7031c08e2a0559ecd846ecebdd
ysrhlmky <yasarhalimkaya@gmail.com>
2016-05-01 11:48:08 +0300

    Add a check to see if system supports recording

--- app/src/main/java/kurufasulye/synthesizer/OpenSLPlayer.java
+++ app/src/main/java/kurufasulye/synthesizer/OpenSLPlayer.java
@@ -1,17 +1,33 @@
 package kurufasulye.synthesizer;
 
+import android.content.Context;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.util.Log;
+
 /**
  * A <code>Player</code> object implemented using
  * native OpenSL apis
  */
 public class OpenSLPlayer implements Player {
+    private static final String TAG = "OpenSLPlayer";
 
-    static {
-        System.loadLibrary("openslplayerjni");
+    private Context context;
+    private String nativeSampleRate;
+    private String nativeSampleBufferSize;
+
+    public OpenSLPlayer(Context context) {
+        this.context = context;
     }
 
     @Override
     public boolean start() {
+        if (!isSystemSupportRecording()) {
+            Log.e(TAG, "System does not support recording");
+            return false;
+        }
+
         return init();
     }
 
@@ -23,6 +39,31 @@ public class OpenSLPlayer implements Player {
     @Override
     public void mute(boolean isMute) {
 
+    }
+
+    private boolean isSystemSupportRecording() {
+        AudioManager audioManager = (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
+
+        nativeSampleRate = audioManager.getProperty(AudioManager.PROPERTY_OUTPUT_SAMPLE_RATE);
+        nativeSampleBufferSize = audioManager.getProperty(AudioManager.PROPERTY_OUTPUT_FRAMES_PER_BUFFER);
+
+        int recordBufferSize = AudioRecord.getMinBufferSize(
+                Integer.parseInt(nativeSampleRate),
+                AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT);
+
+        if (recordBufferSize == AudioRecord.ERROR || recordBufferSize == AudioRecord.ERROR_BAD_VALUE) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Load native library
+     */
+    static {
+        System.loadLibrary("openslplayerjni");
     }
 
     /**
