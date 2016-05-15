56022693d1bef1beded310eada2676b0387b01da
ysrhlmky <yasarhalimkaya@gmail.com>
2016-05-15 14:32:23 +0000

    openslplayerjni.c deleted online with Bitbucket

--- app/src/main/jni/openslplayerjni.c
+++
@@ -1,7 +0,0 @@
-#include <jni.h>
-
-JNIEXPORT jboolean JNICALL
-Java_kurufasulye_synthesizer_OpenSLPlayer_init(JNIEnv *env, jobject instance) {
-    jboolean jboolean1 = 1;
-    return jboolean1;
-}
